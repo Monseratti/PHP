612c933072d2c58eab70c1045d381b74f50414b6
Monseratti <dnamaxy@gmail.com>
2023-05-07 18:50:16 +0300

    commit

---
+++ HW2904/index.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <title>Title</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS v5.2.1 -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+
+</head>
+
+<body>
+  <main>
+    <a href="1.php" class="btn btn-outline-primary">Go to 1.php</a>
+    <a href="2.php" class="btn btn-outline-success">Go to 2.php</a>
+
+    <div>
+        <?php
+        if(isset($_COOKIE['p1Visit'])){
+            echo "<p>You visited 1.php {$_COOKIE['p1Visit']} times</p>";
+        }
+        if(isset($_COOKIE['p2Visit'])){
+            echo "<p>You visited 2.php {$_COOKIE['p2Visit']} times</p>";
+        }
+        ?>
+    </div>
+  </main>
+  <!-- Bootstrap JavaScript Libraries -->
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+    integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
+  </script>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js"
+    integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous">
+  </script>
+</body>
+
+</html>
